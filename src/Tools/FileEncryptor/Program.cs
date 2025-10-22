f0088727d20b02899b31a00860714aaeb0852382
herve <simon@dufflecoat.io>
2025-10-22 12:31:27 +1000

    encrypt/decrypt files

---
+++ src/Tools/FileEncryptor/Program.cs
@@ -0,0 +1,62 @@
+﻿using GTMH.Security;
+using GTMH.Util;
+
+Func<int> PrintUsage=()=>
+{
+  Console.WriteLine("Usage:");
+  Console.WriteLine("Optional: --secret=<secret>");
+  Console.WriteLine("Required: --input=<a_file>");
+  Console.WriteLine("Required: --output=<a_file>");
+  Console.WriteLine("Required: --action=<action>");
+  Console.WriteLine("where <action> must be encrypt or decrypt");
+  return -1;
+};
+
+var secret = args.GetCmdLine("secret");
+
+var input = args.GetCmdLine("input");
+if(input == null || !System.IO.File.Exists(input)) return PrintUsage();
+
+var output = args.GetCmdLine("output");
+if ( output==null ) return PrintUsage();
+
+
+if(secret == null)
+{
+  Console.WriteLine("Enter secret");
+  secret=Console.ReadLine();
+  if ( secret == null) return PrintUsage();
+}
+
+try
+{
+  var input_data = System.IO.File.ReadAllBytes(input);
+  byte[] output_data;
+  switch(args.GetCmdLine("action", "unspecified").ToLower())
+  {
+    case "encrypt":
+    {
+      output_data = Cipher.Encrypt(input_data, secret).EncryptedData.ToArray();
+      break;
+    }
+    case "decrypt":
+    {
+      output_data = new Cipher(input_data).Decrypt(secret);
+      break;
+    }
+    default:
+    {
+      return PrintUsage();
+    }
+  }
+  System.IO.File.WriteAllBytes(output, output_data);
+}
+catch(Exception e)
+{
+  Console.WriteLine($"Error in main: {e}");
+  return -2;
+}
+
+
+
+return 0;
